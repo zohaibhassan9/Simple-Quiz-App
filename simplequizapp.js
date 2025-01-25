dcd1c0aec40a39ed44b241d27efd332659ab5901
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-25 21:03:16 +0500

    Questions List

---
+++ simplequizapp.js
@@ -0,0 +1,21 @@
+const questions = [
+    {
+question: "What is the capital of France",
+options: ["Karachi", "Qatr", "Berlin", "France"],
+answer:3,
+    },
+    {
+question: "Which language runs in a web browser?",
+options: ["JS", "C++", "Java", "Pyton"],
+answer: 0,
+   },
+   {
+question: "What does HTML stand for?",
+options: [
+    "Hyper Text Markup Language",
+    "Hot Mail",
+    "How To Make Links",
+    "None of the above",]
+
+   },
+];
